e1a4e02a660a3c9635ec934185f00c89c2440dd9
Tony Blum <blum@college.harvard.edu>
2014-03-14 23:48:33 -0400

    got axis to display

--- Problem2/linegraph.js
+++ Problem2/linegraph.js
@@ -31,10 +31,11 @@
         });
 
 
-    d3.csv("timeline.csv", function(data) {
+    d3.csv("timeline1.csv", function(data) {
 
         // convert your csv data and add it to dataSet
 		console.log(data);
+		console.log("hello");
 
 
         return createVis();
@@ -43,9 +44,14 @@
     createVis = function() {
         var xAxis, xScale, yAxis,  yScale;
 
+		//var width = 100;
+		//var height = 100;
+
           xScale = d3.scale.linear().domain([0,100]).range([0, bbVis.w]);  // define the right domain generically
 
 		  // example that translates to the bottom left of our vis space:
+		
+/*
 		  var visFrame = svg.append("g").attr({
 		      "transform": "translate(" + bbVis.x + "," + (bbVis.y + bbVis.h) + ")",
 		  	  //....
@@ -53,7 +59,8 @@
 		  });
 		  
 		  visFrame.append("rect");
-		  //....
+*/		
+  //....
 		  
 //        yScale = .. // define the right y domain and range -- use bbVis
 
@@ -61,6 +68,44 @@
 //        yAxis = ..
 //        // add y axis to svg !
 
+		var margin = {top: 20, right: 20, bottom: 30, left: 50},
+		width = 960 - margin.left - margin.right,
+		height = 500 - margin.top - margin.bottom;
+
+		var x = d3.time.scale()
+			.range([0, width]);
+
+		var y = d3.scale.linear()
+			.range([height, 0]);
+
+		var xAxis = d3.svg.axis()
+			.scale(x)
+			.orient("bottom");
+
+		var yAxis = d3.svg.axis()
+			.scale(y)
+			.orient("left");
+
+		var svg = d3.select("body").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		  .append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+		  svg.append("g")
+			  .attr("class", "x axis")
+			  .attr("transform", "translate(0," + height + ")")
+			  .call(xAxis);
+
+		  svg.append("g")
+			  .attr("class", "y axis")
+			  .call(yAxis)
+			.append("text")
+			  .attr("transform", "rotate(-90)")
+			  .attr("y", 6)
+			  .attr("dy", ".71em")
+			  .style("text-anchor", "end")
+			  .text("Price ($)");
 		
 
 
